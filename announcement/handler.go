0a9e0a70be3f5b3b6ee43c1e7c23ae85b4166f5b
wannabea <350663371@qq.com>
2022-05-07 17:50:58 +0800

    first commit

--- announcement/handler.go
+++ announcement/handler.go
@@ -19,28 +19,30 @@ type ApiImpl struct{}
 // GetAnnouncementById implements the ApiImpl interface.
 
 type announcementFromDb struct {
-	AnnounceId    	int
-	Title          	string 
-	UserId       	string 
-	Content        	string 
-	CreateTime     	int 
-	LastUpdateTime 	int 
+	AnnounceId     int
+	Title          string
+	UserId         int
+	Content        string
+	CreateTime     int
+	LastUpdateTime int
 }
 
 func (s *ApiImpl) GetAnnouncementById(ctx context.Context, req *announcement.AnnounceRequest) (resp *announcement.AnnounceResponse, err error) {
-	var announce announcementFromDb 
+	var announce announcementFromDb
 	sqlStr := "select * from announcement where id=?"
 	err := db.QueryRow(sqlStr, 1).Scan(&announce.AnnounceId, &announce.Title, &announce.UserId, &announce.Content, &announce.CreateTime, &announce.LastUpdateTime)
-	
+
 	if err != nil {
 		fmt.Printf("scan failed, err:%v\n", err)
 		return
-	} 
+	}
 	resp = &announcement.AnnounceResponse{
-		Title: announce.Title,
-		UserName: strconv.Itoa(announce.UserId),
-		Content: announce.Content,
-
+		AnnounceId:     req.AnnounceId,
+		Title:          announce.Title,
+		UserName:       strconv.Itoa(announce.UserId),
+		Content:        announce.Content,
+		CreateTime:     strconv.Itoa(announce.CreateTime),
+		LastUpdateTime: strconv.Itoa(announce.LastUpdateTime),
 	}
 	return
 }

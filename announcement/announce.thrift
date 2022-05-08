9ae870ea2d98a61a41a4208b27f5df540497f36a
wannabea <350663371@qq.com>
2022-05-08 11:46:58 +0800

    update announce idl

--- announcement/announce.thrift
+++ announcement/announce.thrift
@@ -1,10 +1,16 @@
 namespace go announce
 
+struct Identity {
+	1: optional i32 userId
+	2: optional string address
+	3: optional i32 isAdmin
+}
+
 struct AnnounceRequest {
   1: i32 announce_id
 }
 
-struct AnnounceResponse {
+struct AnnounceInfo {
   1: i32 announceId
   2: string title
   3: string userName
@@ -14,7 +20,11 @@ struct AnnounceResponse {
 }
 
 service Api {
-    AnnounceResponse GetAnnouncementById (1: AnnounceRequest req)
-	list<AnnounceResponse> GetAllAnnouncements ()
+    AnnounceInfo GetAnnouncementById (1: AnnounceRequest req)
+	list<AnnounceInfo> GetAllAnnouncements ()
+	i32 InsertAnnouncement(1: AnnounceInfo, 2: Identity)
+	i32 UpdateAnnouncement(1: AnnounceInfo, 2: Identity)
+	i32 DeleteAnnouncement(1: AnnounceInfo, 2: Identity)
+	i32 HideAnnouncement(1: AnnounceInfo, 2: Identity)
 }
 
